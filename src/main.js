1eeef69529ab455dd8425fe465231bff7dde46f3
RafalKornel <rafal.kornel123@gmail.com>
2020-09-19 18:23:30 +0200

    Slightly impoved performance by not drawing already drawn elements

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-var   control   = true;
-const canvas    = document.querySelector("canvas");
-const c         = canvas.getContext("2d");
-const clearBtn  = document.querySelector("input");
-var   figures   = [];
+var control = true;
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+const clearBtn = document.querySelector("input");
+var figures = [];
+const resMod = 2;
 
 
 function setSize() {
-    canvas.width = window.innerWidth * 0.8;
-    canvas.height = window.innerHeight * 0.8;
+    canvas.width = window.innerWidth  * resMod;
+    canvas.height = window.innerHeight  * resMod;
 }
 
 setSize();
@@ -19,17 +20,22 @@ class Figure {
     constructor(type) {
         this.points = [];
         this.type = type;
+        this.strokeWidth = 10;
     }
 
     draw() {
         c.beginPath();
-        for (let i = 0; i < this.points.length - 1; i++) {
-            let current = this.points[i];
-            let next    = this.points[i + 1];
-            c.moveTo(current.x, current.y);
-            c.lineTo(next.x, next.y);
+        c.lineWidth = this.strokeWidth;
+        if (this.points.length == 1) {
+            c.fillRect(this.points[0].x - 1, this.points[0].y - 1, 2, 2)
+        }
+        else {
+            let l = 
+            c.moveTo(this.points[this.points.length-1].x, this.points[this.points.length-1].y);
+            c.lineTo(this.points[this.points.length-2].x, this.points[this.points.length-2].y);
             c.stroke();
         }
+
     }
 }
 
@@ -43,35 +49,30 @@ class Point {
 
 
 var mouseDown = 0;
-var fig = new Figure("line");    
-document.onmousedown  = () => { 
-    fig = new Figure("line");    
-    mouseDown++; 
+var fig = new Figure("line");
+document.onmousedown = () => {
+    console.log("down")
+    fig = new Figure("line");
+    mouseDown = 1;
 }
-document.onmouseup    = () => { 
+document.onmouseup = () => {
     console.log('up');
-    mouseDown--;
+    mouseDown = 0;
     fig.draw();
     figures.push(fig);
-}   
+}
 
-canvas.onmousemove  = (e) => {
+document.onmousemove = (e) => {
     let i = 0;
 
     if (mouseDown) {
-        console.log("down")
-        console.log(fig);
-        //c.clearRect(0, 0, canvas.width, canvas.height);
-        fig.points.push( new Point(e.layerX, e.layerY, i++));
+        fig.points.push(new Point(e.clientX * resMod, e.clientY * resMod, i++));
+        fig.draw();
     }
 
-    fig.draw();
 }
 
-canvas.onmouseleave = 
-
-
-
+//canvas.onmouseleave = document.onmouseup;
 
 function draw() {
 
@@ -84,8 +85,8 @@ function update() {
 
 function gameLoop() {
 
-        update();
-        draw();
+    update();
+    draw();
 }
 
 //var loop = setInterval(gameLoop, deltaTime);
